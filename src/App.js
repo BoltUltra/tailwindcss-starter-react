b036f48d409a45e10b48ab86ade0e82f72dd5bd7
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-10-28 09:01:18 +0100

    made changes in the App.js and README.md file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const App = () => {
   return (
     <div>
-      <p>App</p>
+      <p className="text-3xl font-bold capitalize">
+        This is react starter with tailwind
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
